b237f602086d539b3b2580a262a757bbe11096ba
pcetec <pc@etec.com>
2026-09-21 22:38:12 -0300

    Calculadora de Média

--- app/src/main/java/com/example/aplicativopan/MainActivity.kt
+++ app/src/main/java/com/example/aplicativopan/MainActivity.kt
@@ -3,23 +3,29 @@ package com.example.aplicativopan
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class  MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.aulaid)
 
-        val botao = findViewById<Button>(R.id.btnProxima)
-        val texto = findViewById<EditText>(R.id.editValor)
+        val notap1 = findViewById<EditText>(R.id.editNota1)
+        val notap2 = findViewById<EditText>(R.id.editNota2)
+        val resultado = findViewById<TextView>(R.id.txtResultado)
+        val botao = findViewById<Button>(R.id.btnCalcular)
 
-        botao.text = "Testando o botao"
 
         botao.setOnClickListener {
-            botao.text = texto.text
+            resultado.text = "Média é " +
+                                ((notap1.text.toString().toFloat()
+                                + notap2.text.toString().toFloat()
+                                )
+                                /2 ).toString()
 
 
         }
